48b809c5847f0cf631132a99cfc5da9e83dcce99
Vivek Pandya <vivekvpandya@gmail.com>
2015-10-09 02:15:39 +0530

    Adding the missing comment files.

---
+++ Drupal8iOS/Comment.m
@@ -0,0 +1,13 @@
+//
+//  Comment.m
+//  Drupal8iOS
+//
+//  Created by Vivek Pandya on 9/21/15.
+//  Copyright © 2015 PikLips. All rights reserved.
+//
+
+#import "Comment.h"
+
+@implementation Comment
+
+@end
